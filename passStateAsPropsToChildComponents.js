23bbf1ba0ca198af219bd974ac3fa64bb5051fe3
Akash <akashleo2009@gmail.com>
2021-09-14 11:02:55 +0530

    pass state as props to child elements

--- passStateAsPropsToChildComponents.js
+++ passStateAsPropsToChildComponents.js
@@ -2,31 +2,34 @@ class MyApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "CamperBot",
-    };
+      name: 'CamperBot'
+    }
   }
   render() {
     return (
-      <div>
-        {/* Change code below this line */}
-        <Navbar />
-        {/* Change code above this line */}
-      </div>
+       <div>
+         {/* Change code below this line */}
+         <Navbar name={this.state.name} />
+         {/* Change code above this line */}
+       </div>
     );
   }
-}
+};
 
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
+    console.log(props);
   }
   render() {
+  
     return (
-      <div>
-        {/* Change code below this line */}
-        <h1>Hello, my name is: </h1>
-        {/* Change code above this line */}
-      </div>
+    <div>
+      {/* Change code below this line */}
+      <h1>Hello, my name is:
+      <span>{this.props.name}</span></h1>
+      {/* Change code above this line */}
+    </div>
     );
   }
-}
+};
